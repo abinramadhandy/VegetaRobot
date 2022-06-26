0bbc2b3a5678f9651f12f8f64defe7c6ac1cb8f3
abinramadhandy <96660796+abinramadhandy@users.noreply.github.com>
2022-06-26 21:19:04 +0700

    Update handlers.py

--- VegetaRobot/modules/helper_funcs/handlers.py
+++ VegetaRobot/modules/helper_funcs/handlers.py
@@ -1,6 +1,6 @@
-import VegetaRobot.modules.sql.blacklistusers_sql as sql
-from VegetaRobot import ALLOW_EXCL
-from VegetaRobot import (DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES)
+import LuffyRobot.modules.sql.blacklistusers_sql as sql
+from LuffyRobot import ALLOW_EXCL
+from LuffyRobot import (DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES)
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
