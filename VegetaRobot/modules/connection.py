8116bdfdee84ebe41d751262767087b244c6be80
abinramadhandy <96660796+abinramadhandy@users.noreply.github.com>
2022-06-26 20:45:55 +0700

    Update connection.py

--- VegetaRobot/modules/connection.py
+++ VegetaRobot/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 
-import VegetaRobot.modules.sql.connection_sql as sql
-from VegetaRobot import dispatcher, DRAGONS, DEV_USERS
-from VegetaRobot.modules.helper_funcs import chat_status
-from VegetaRobot.modules.helper_funcs.alternate import send_message, typing_action
+import LuffyRobot modules.sql.connection_sql as sql
+from LuffyRobot import dispatcher, DRAGONS, DEV_USERS
+from LuffyRobot.modules.helper_funcs import chat_status
+from LuffyRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
